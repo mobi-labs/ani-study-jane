2f2a09a51d05d30bf56df61a673196ff04c9e3f2
JeongwooHam <hamjw0122@gmail.com>
2024-10-27 21:55:07 +0900

    🔧 feat: exports 경로 설정

--- code/install-test/src/App.tsx
+++ code/install-test/src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <Button />
+        <Button label={"성공~~~^__^"} />
       </header>
     </div>
   );
